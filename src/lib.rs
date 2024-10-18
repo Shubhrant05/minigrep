b0f9615d8111b3305b017141e51c0269680419ea
Shubhrant Yadav <shubhrantyadav34366@gmail.com>
2024-10-19 00:02:45 +0530

    did error handling, added color, added case sensitive search flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
-
+use std::process;
+use colored::Colorize;
 pub struct Config {
     pub word_to_find: String,
     pub filename: String
@@ -9,7 +10,7 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String] ) -> Result<Config, &str> {
         if args.len() < 3 {
-           return Err("[ERROR] : Not enough arguments");
+           return Err("Not enough arguments");
         }
         let query = &args[1];
         let filename = &args[2];
@@ -17,21 +18,32 @@ impl Config {
     }
 }
 
-pub fn run(config : Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(&config.filename)?;
-
-    for line in contents.split('.') {
+fn search(word_to_find: &str, contents: &str, case_sensitive: bool) -> Result<(), Box<dyn Error>> {
+    let mut result = Vec::new();
+    for line in contents.split('.'){
         let line = line.trim();
-        if line.is_empty() {
-            continue;
-        }
-        if line.contains(&config.word_to_find) {
-            println!("Found! [ {} ] in the line [ {} ]", &config.word_to_find, line);
+        let (search_line, search_word) = if case_sensitive {
+            (line.to_string(), word_to_find.to_string()) 
+        } else {
+            (line.to_lowercase(), word_to_find.to_lowercase()) 
+        };
+        if !line.is_empty() && search_line.contains(&search_word) {
+            result.push(line);
+            println!("{} [ {} ] in the line [ {} ]","Found!".green().bold(), word_to_find.yellow().bold(), line.white().dimmed());
             return Ok(());
         }
     }
+    println!("{} [ {} ] in the the file","Not Found!".red().bold(), word_to_find.yellow().bold());
+    return Ok(());
+}
 
-    println!("Not found [ {} ] in the file [ {} ]", &config.word_to_find, &config.filename);
+pub fn run(config : Config, case_sensitive: bool) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.filename)?;
+
+    if let Err(res) = search(&config.word_to_find, &contents, case_sensitive) {
+        println!("{}", res);
+        process::exit(1);
+    }
     return Ok(());
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 use std::env;
 use std::process;
 use minigrep::Config;
+use colored::Colorize;
+
 fn main() {
 
     let args: Vec<String> = env::args().collect();
+    let mut case_sensitive: bool = false;
+
+
+    if args.len() > 3 && args.contains(&String::from("--case_sensitive")) {
+        case_sensitive = true;
+    }
+    else if args.len() > 3 && !args.contains(&String::from("--case_sensitive")) {
+        println!("{} Please specify the --case_sensitive flag", "ERROR: ".red().bold());
+        process::exit(1);
+    }
     let arg_config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     
-    println!("Searching for [ {} ] in [ {} ]", arg_config.word_to_find, arg_config.filename);
+    println!("Searching for [ {} ] in [ {} ].......", arg_config.word_to_find, arg_config.filename);
 
-    if let Err(res) = minigrep::run(arg_config) {
+    if let Err(res) = minigrep::run(arg_config, case_sensitive) {
         println!("{}",res);
         process::exit(1);
     }
